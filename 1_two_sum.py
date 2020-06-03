275215e84d8b543ff8ebeae1b2ae34d3be62a5cd
mshivaku99 <54600104+mshivaku99@users.noreply.github.com>
2020-06-03 03:24:26 -0400

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,17 +1,3 @@
-""" 
-Leetcode #1, easy, array, hash table; 1/24/2019
-Given an array of integers, return indices of the two numbers such that they 
-add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may 
-not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9, because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-"""
-
 class Solution(object):
     def twoSum(self, nums, target):
         """
